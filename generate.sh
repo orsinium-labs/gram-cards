717b9fdf8d92bcdd0c6e55d5b721b6e7b587d283
Gram <master_fess@mail.ru>
2020-10-29 14:32:20 +0100

    join cards

--- generate.sh
+++ generate.sh
@@ -1,4 +1,9 @@
-mkdir -p ./build/
+#!/usr/bin/env bash
+
+out="build"
+mkdir -p "$out"
+
+# generate cards
 n=0
 cat phrases.txt | while read phrase; do
   n=$((n+1))
@@ -9,9 +14,28 @@ cat phrases.txt | while read phrase; do
     -font Ubuntu-Title \
     -extent 380x530 \
     -size 360x530 \
+    -bordercolor gray \
+    -border 2x2 \
     -pointsize 56 \
     -gravity center \
     caption:"$phrase" \
-    "./build/$n.png"
+    "$out/$n.png"
 done
+
+
+# join cards
+
+convert \
+    '(' $out/{1,2,3}.png +append ')' \
+    '(' $out/{4,5,6}.png +append ')' \
+    '(' $out/{7,8,9}.png +append ')' \
+    -background none \
+    -append $out/result-01-09.png
+
+convert \
+    '(' $out/{10,11,12}.png +append ')' \
+    '(' $out/{13,14,15}.png +append ')' \
+    '(' $out/{16,17,18}.png +append ')' \
+    -background none \
+    -append $out/result-10-18.png
 
